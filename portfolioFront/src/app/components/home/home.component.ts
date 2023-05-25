2db5f86509ab4d2e522c1a6fc8fb350434d1fb3f
Fran Ortega <ortegfran@gmail.com>
2023-05-25 13:30:50 +0200

    component home

--- portfolioFront/src/app/components/home/home.component.ts
+++ portfolioFront/src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+
+  showPerfil = false
+
+
+  updateImageVisibility() {
+    if (window.innerWidth <= 768) {
+      this.showPerfil = true;
+    } else {
+      this.showPerfil = false;
+    }
+  }
 }
+
