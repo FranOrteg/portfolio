89f971b23213e10667db6c0d61a929a582aeaf24
Fran Ortega <ortegfran@gmail.com>
2023-05-25 13:27:51 +0200

    component about

--- portfolioFront/src/app/components/about/about.component.ts
+++ portfolioFront/src/app/components/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Skills } from 'src/app/interfaces/skills';
+import { SkillService } from 'src/app/services/skill.service';
 
 @Component({
   selector: 'app-about',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  skills: Skills[]
+
+  constructor(private skillService: SkillService) {
+    this.skills = []
+  }
+
+  ngOnInit() {
+    this.skills = this.skillService.getAll()
+  }
 }
