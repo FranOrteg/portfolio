a8425a56a8702ba54b06d60311a1a413bebfbf80
Fran Ortega <ortegfran@gmail.com>
2023-05-25 13:02:21 +0200

    components

--- portfolioFront/src/app/app.module.ts
+++ portfolioFront/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectViewComponent } from './components/project-view/project-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProjectsComponent,
+    ProjectViewComponent
   ],
   imports: [
     BrowserModule,
